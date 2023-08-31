eae12ad50a5955f436f2184b45745a70a4202af2
ilia03071993 <celiwan@mail.ru>
2023-08-31 13:51:40 +0300

    Change in feature2

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,7 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("for commit");
-        System.out.println("Feature");
+        System.out.println("Feature1");
+        System.out.println("Feature2");
     }
 }
