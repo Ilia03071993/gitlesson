f60d579a821a364b578d43a246120ad5e216e0cf
ilia03071993 <celiwan@mail.ru>
2023-08-31 12:48:16 +0300

    Add Test class

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello from Test");
-        int i = 123;
+        System.out.println("test");
     }
 }
